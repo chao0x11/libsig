ee095572cea5d6ade7551b8fbb23d354a6729d28
chao <chaosafeai@gmail.com>
2024-12-18 10:10:35 +0800

    Add timing function

--- libsig-sign.cc
+++ libsig-sign.cc
@@ -10,49 +10,60 @@
 
 #include <chrono>
 
-std::string getSuffix(const std::string& fileName) {
-    std::string suffix;
-    int mark = 0;
-    for (int i = fileName.size()-1; i >= 0; --i) {
-        if (fileName[i] == '.') {
-            mark = i;
-            break;
-        }
-    }
-    for (int i = mark; i < fileName.size(); ++i) {
-        suffix.push_back(fileName[i]);
-    }
-    return suffix;
-}
-
-void readFile(const char* fileName, std::vector<unsigned char>& data) {
-    std::ifstream file(fileName, std::ios::binary | std::ios::ate);
-    std::streamsize size = file.tellg();
-    file.seekg(0, std::ios::beg);
-    data.resize(size);
-    file.read(reinterpret_cast<char*>(data.data()), size);
-    file.close();
-}
-
 int main(int argc, char* argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
 
     std::string media = argv[1];
-    std::string private_key = argv[2];
-    int identity_id = atoi(argv[3]);
+    std::string public_key = argv[2];
+
+    std::ifstream inFile(media, std::ios::binary);
+    if (!inFile) {
+        std::cerr << "Open file error: " << media << std::endl;
+        return 1;
+    }
+
+    // Read signature size
+    inFile.seekg(0, std::ios::end);
+    std::streampos fileSize = inFile.tellg();
+    inFile.seekg((int)fileSize - sizeof(int), std::ios::beg);
+    int total_size;
+    inFile.read(reinterpret_cast<char*>(&total_size), sizeof(total_size));
+    int media_size = (int)fileSize - total_size;
+
+    inFile.seekg(0, std::ios::beg);
+    std::vector<unsigned char> media_data(media_size);
+    inFile.read(reinterpret_cast<char*>(media_data.data()), media_data.size());
+    int identity_id;
+    inFile.read(reinterpret_cast<char*>(&identity_id), sizeof(identity_id));
+    int signature_size;
+    inFile.read(reinterpret_cast<char*>(&signature_size), sizeof(signature_size));
+    std::vector<unsigned char> signature(signature_size);
+    inFile.read(reinterpret_cast<char*>(signature.data()), signature.size());
+
+    std::cout << "indentity id: " << identity_id << std::endl;
+
+    // Write signature and media data into tmp files
+    std::ofstream sig_file("tmp-signature.bin", std::ios::binary);
+    sig_file.write(reinterpret_cast<char*>(signature.data()), signature.size());
+    std::ofstream media_file("tmp-media.bin", std::ios::binary);
+    media_file.write(reinterpret_cast<char*>(media_data.data()), media_data.size());
+
+    sig_file.close();
+    media_file.close();
 
     auto openssl_start = std::chrono::high_resolution_clock::now();
 
-    std::string command = "openssl dgst -sha256 -sign ";
-    command = command + private_key;
-    command = command + " -out signature.bin ";
-    command = command + media;
- 
-    int ret = std::system(command.c_str());
+    // Verify signature using openssl
+    std::string command = "openssl dgst -sha256 -verify ";
+    command = command + public_key;
+    command = command + " -signature tmp-signature.bin tmp-media.bin";
 
-    if (ret !=0) {
-        std::cerr << "Error sign file." << std::endl;
+    int ret = std::system(command.c_str());
+    if (ret != 0) {
+        std::cerr << "Verification failed." << std::endl;
         return 1;
+    } else {
+        std::cout << "Verification successful." << std::endl;
     }
 
     auto openssl_end = std::chrono::high_resolution_clock::now();
@@ -63,36 +74,15 @@ int main(int argc, char* argv[]) {
     
     std::cout << "OpenSSL execution time: " << openssl_milliseconds << " ms\n";
 
-    // Read signature and media content
-    std::vector<unsigned char> signature_data;
-    std::vector<unsigned char> media_data;
-    readFile("signature.bin", signature_data);
-    readFile(media.c_str(), media_data);
-
-    // Write media data and signature into a new file
-    std::string suffix = getSuffix(media);
-    std::string new_file_path = "signed" + suffix;
-    std::ofstream outFile(new_file_path, std::ios::binary);
-    if (!outFile) {
-        std::cerr << "Cannot creat a output file: " << new_file_path << std::endl;
+    ret = std::system("rm tmp-signature.bin");
+    if (ret != 0) {
+        std::cerr << "Error remove tmp-signature.bin" << std::endl;
         return 1;
     }
 
-    int sig_size = signature_data.size();
-    int total_size = 12 + sig_size;
-    int identity = identity_id;
-    outFile.write(reinterpret_cast<char*>(media_data.data()), media_data.size());
-    outFile.write(reinterpret_cast<char*>(&identity), sizeof(identity_id));
-    outFile.write(reinterpret_cast<char*>(&sig_size), sizeof(sig_size));
-    outFile.write(reinterpret_cast<char*>(signature_data.data()), signature_data.size());
-    outFile.write(reinterpret_cast<char*>(&total_size), sizeof(total_size));
-
-    std::cout << media << " has been signed and save to: " << new_file_path << std::endl;
-
-    ret = std::system("rm signature.bin");
-
+    ret = std::system("rm tmp-media.bin");
     if (ret != 0) {
-        std::cerr << "Error remove tmp file." << std::endl;
+        std::cerr << "Error remove tmp-media.bin" << std::endl;
         return 1;
     }
 
